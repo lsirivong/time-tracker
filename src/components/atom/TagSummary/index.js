beea6b078f6bb301c4e5b65c46e1479c7a62ac2e
Lenny Sirivong <lenny@lenny-s.com>
2017-02-04 16:58:46 -0500

    Show total as decimal

--- src/components/atom/TagSummary/index.js
+++ src/components/atom/TagSummary/index.js
@@ -2,21 +2,32 @@ import React from 'react';
 import Duration from '../Duration/';
 import DurationDecimal from '../DurationDecimal/';
 
-const TagSummary = ({ tags = {} }) => (
-  <code>
-    {Object.keys(tags).map(tagName =>
-      <div key={tagName}>
-        {tagName}: <Duration value={tags[tagName].duration} /> (<DurationDecimal value={tags[tagName].duration} />)
+const TagSummary = ({ tags = {} }) => { 
+  const totalDuration = Object.keys(tags).reduce(
+    (result, tagName) => result + tags[tagName].duration,
+    0
+  );
+
+  return (
+    <code>
+      {Object.keys(tags).map(tagName =>
+        <div key={tagName}>
+          {tagName}:
+          {' '}
+          <Duration value={tags[tagName].duration} />
+          {' '}
+          (<DurationDecimal value={tags[tagName].duration} />)
+        </div>
+      )}
+      <div key="total">
+        <strong>Total</strong>:
+        {' '}
+        <Duration value={totalDuration} />
+        {' '}
+        (<DurationDecimal value={totalDuration} />)
       </div>
-    )}
-    <div key="total">
-      <strong>Total</strong>: <Duration
-        value={Object.keys(tags).reduce(
-          (result, tagName) => result + tags[tagName].duration,
-          0
-        )} />
-    </div>
-  </code>
-);
+    </code>
+  );
+};
 
 export default TagSummary;
